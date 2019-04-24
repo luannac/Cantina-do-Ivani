bcfb139492cc6a62d17e0846ef0d79e3078cb58f
Luann Campos <luann-ac@outlook.com>
2019-04-24 11:41:56 -0300

    Site e Diagrama de Classe

---
+++ Aplicação Desktop/CantinaDaIvani/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CantinaDaIvani.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string serverAdress = "Server=ESN509VMSSQL;Database=2019_3it_LuannCampos;User id=Aluno;Password=Senai1234";
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            //saves on Database
+            SqlConnection con = new SqlConnection(serverAdress);
+
+            con.Open();
+            SqlCommand query = new SqlCommand("SELECT * FROM Usuario WHERE nickname = @name and password = @password ", con);
+
+            SqlDataReader reader = query.ExecuteReader();
+            if (reader.Read())
+            {
+                con.Close();
+                return RedirectToAction("Index");
+            }
+            query.ExecuteNonQuery();
+            con.Close();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
